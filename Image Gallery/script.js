693df9f5b2467b64cd41d0e1a0be52e752b46df8
Aswinar01 <aswinar2k03@gmail.com>
2024-09-17 19:48:25 +0530

    Add files via upload

---
+++ Image Gallery/script.js
@@ -0,0 +1,29 @@
+let slideIndex = 1;
+
+function openLightbox() {
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("gallery-item");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
